0f098571c3204ecdda7fd4a9b7c70ac1a9d020f5
archibate <1931127624@qq.com>
2020-08-02 08:31:27 +0800

    helper func

--- taichi.js
+++ taichi.js
@@ -1,12 +1,33 @@
+if (window && window.location.href.startsWith('file://')) {
+    alert('Taichi.js could not be functional when accessed from a local '
+        + 'file (i.e. file://) due to WASM limitation, please run '
+        + '`python server.py` in the project root directory and access '
+        + 'this page using http or https instead.');
+}
+
 class Taichi {
     constructor(module) {
         this.module = module;
     }
 
+    set_arg_int(index, value) {
+        let args_base = this.module._Ti_args/4 + index * 2;
+        let args = this.module.HEAP32.subarray(args_base, args_base + 1);
+
+        args[0] = value;
+    }
+
+    set_arg_float(index, value) {
+        let args_base = this.module._Ti_args/4 + index * 2;
+        let args = this.module.HEAPF32.subarray(args_base, args_base + 1);
+
+        args[0] = value;
+    }
+
     set_ext_arr(index, shape) {
         let earg_base = this.module._Ti_earg/4 + index * 8;
         let extr_base = this.module._Ti_extr/4;
-        let args_base = this.module._Ti_args/4 + index;
+        let args_base = this.module._Ti_args/4 + index * 2;
         let earg = this.module.HEAP32.subarray(earg_base, earg_base + 8);
         let args = this.module.HEAP32.subarray(args_base, args_base + 1);
 
@@ -35,4 +56,38 @@ class Taichi {
     ready(cb) {
         this.module.onRuntimeInitialized = cb;
     }
+}
+
+function taichiAnimation(callback) {
+    let fps = 0;
+    let last_time = Date.now();
+
+    function wrapped() {
+        window.requestAnimationFrame(wrapped);
+
+        if((Date.now() - last_time) >= 1000) {
+            console.log(fps, 'FPS');
+            last_time = Date.now();
+            fps = 0;
+        }
+        fps++;
+
+        callback();
+    }
+    wrapped();
+}
+
+function taichiSetImage(ctx, image) {
+    let imgData = ctx.createImageData(RES, RES);
+    for (let y = 0; y < RES; y++) {
+        for (let x = 0; x < RES; x++) {
+            let i = (y * RES + x) * 4;
+            let j = (x * RES + (RES - 1 - y)) * 4;
+            imgData.data[i++] = parseInt(image[j++] * 255);
+            imgData.data[i++] = parseInt(image[j++] * 255);
+            imgData.data[i++] = parseInt(image[j++] * 255);
+            imgData.data[i++] = 255 - parseInt(image[j++] * 255);
+        }
+    }
+    ctx.putImageData(imgData, 0, 0);
 }

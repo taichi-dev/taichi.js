498bd7261eb644d0eaf92bcb1fdedd3cdae61040
archibate <1931127624@qq.com>
2020-09-25 22:16:59 +0800

    add frontend and cache

--- taichihub/compiler.py
+++ taichihub/compiler.py
@@ -2,38 +2,88 @@
 
 import subprocess
 import tempfile
+import random
+import string
+import shutil
+import time
 import sys
 import os
 
 
-def do_compile(program):
+def do_compile(target, source=None):
+    if source is None:
+        source = target
+
     print('Generating action record...')
     env = dict(os.environ)
-    env.update({'TI_ACTION_RECORD': f'{program}.yml', 'TI_ARCH': 'cc'})
-    subprocess.check_call([sys.executable, program], env=env)
+    env.update({'TI_ACTION_RECORD': f'{target}.yml', 'TI_ARCH': 'cc'})
+    try:
+        output = subprocess.check_output([sys.executable, '-m', 'taichi', 'run', source],
+            env=env, stderr=subprocess.STDOUT, timeout=8)
+    except subprocess.CalledProcessError as e:
+        print('Error while generating action record:')
+        print(e.output)
+        print('(END)')
+        os.unlink(f'{target}.yml')
+        return e.output, 'failure'
+    except subprocess.TimeoutExpired as e:
+        print('Timeout while generating action record:')
+        print(e.output)
+        print('(END)')
+        os.unlink(f'{target}.yml')
+        return e.output + '\n(Time limit exceed)', 'timeout'
+
     print('Composing C file...')
-    subprocess.check_output([sys.executable, '-m', 'taichi', 'cc_compose', '-e', f'{program}.yml', f'{program}.c', f'{program}.h'])
+    subprocess.check_call([sys.executable, '-m', 'taichi', 'cc_compose',
+        '-e', f'{target}.yml', f'{target}.c', f'{target}.h'])
+
     print('Compiling via Emscripten...')
-    subprocess.check_call(['emcc', '-O3', f'{program}.c', '-o', f'{program}.js'])
+    subprocess.check_call(['emcc', '-O3', f'{target}.c', '-o', f'{target}.js'])
+
+    # https://stackoverflow.com/questions/38769103/document-currentscript-is-null
+    # AJAX loaded Javascript doesn't seems support document.currentScript.src,
+    # which is being used in Emscripten generated JS stub.
+    # So we do a quick hack to make AJAX happy:
+    with open(f'{target}.js') as f:
+        s = f.read()
+    s = s.replace('var scriptDirectory=""', 'var scriptDirectory="/cache/"', 1)
+    with open(f'{target}.js', 'w') as f:
+        f.write(s)
+
+    os.unlink(f'{target}.yml')
+    os.unlink(f'{target}.h')
+    os.unlink(f'{target}.c')
+
+    return output, 'success'
 
 
-def compile_code(source):
+random.seed(time.time_ns())
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+def compile_code(cachedir, source):
+    cacheid = '0.' + get_random_string(10)
+
     with tempfile.TemporaryDirectory() as tmpdir:
-        main_py = os.path.join(tmpdir, 'main.py')
-        main_js = os.path.join(tmpdir, 'main.py.js')
-        main_wasm = os.path.join(tmpdir, 'main.py.wasm')
+        src = os.path.join(tmpdir, 'main.py')
+        dst = os.path.join(cachedir, f'{cacheid}')
 
-        with open(main_py, 'w') as f:
+        with open(src, 'w') as f:
             f.write(source)
 
-        print('compiling', main_py)
-        do_compile(main_py)
-        print('done with', main_py)
+        print('compiling', src, 'to', dst)
+        output, status = do_compile(dst, src)
+        print('done with', src, 'to', dst)
 
-        with open(main_js, 'r') as f:
-            js_code = f.read()
-
-        return js_code
+        output = output.decode()
+        ret = {'status': status, 'output': output}
+        if status == 'success':
+            ret['cacheid'] = cacheid
+            ret['javascript'] = f'/cache/{cacheid}.js'
+        return ret
 
 
 if __name__ == '__main__':
--- taichihub/flaskapp.py
+++ taichihub/flaskapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, Markup
+from flask import Flask, request, render_template, send_from_directory
 from .compiler import compile_code
 import os
 
@@ -17,8 +17,21 @@ def favicon():
             'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
+def get_cache_dir():
+    cachedir = os.path.join(app.root_path, 'cache')
+    if not os.path.exists(cachedir):
+        os.mkdir(cachedir)
+    return cachedir
+
+
 @app.route('/upload', methods=['POST'])
 def upload():
     code = request.form['code']
-    result = compile_code(str(code))
-    return '<code>%s</code>' % Markup.escape(result)
+    result = compile_code(get_cache_dir(), str(code))
+    return result
+
+
+@app.route('/cache/<file>')
+def cache(file):
+    assert file.startswith('0.') and (file.endswith('js') or file.endswith('wasm'))
+    return send_from_directory(os.path.join(app.root_path, 'cache'), file)
